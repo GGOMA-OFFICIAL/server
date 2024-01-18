e9b1f808f8502f3885386885a64dfe83ce885769
Hwangseoeun <sunny030921@gmail.com>
2024-01-18 15:26:29 +0900

    init commit

---
+++ src/main/java/ggoma/ggoma/GgomaApplication.java
@@ -0,0 +1,13 @@
+package ggoma.ggoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ggoma/ggoma/GgomaApplicationTests.java
@@ -0,0 +1,13 @@
+package ggoma.ggoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
