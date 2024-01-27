662e5dbab202b8c9874c2df1bbf3eea5d8230f22
hwangseoeun <sunny030921@gmail.com>
2024-01-28 00:21:28 +0900

    test

--- src/main/java/ggoma/ggoma/GgomaApplication.java
+++ src/main/java/ggoma/ggoma/GgomaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GgomaApplication {
 
 	public static void main(String[] args) {
+		//test
 		SpringApplication.run(GgomaApplication.class, args);
 	}
 
