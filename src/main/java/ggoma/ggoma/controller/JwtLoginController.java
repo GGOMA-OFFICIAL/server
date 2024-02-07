2a1989bdac52a320f8950370ef7870903e1ae39d
Hwangseoeun <sunny030921@gmail.com>
2024-02-08 02:26:48 +0900

    Jwt modification

--- src/main/java/ggoma/ggoma/controller/JwtLoginController.java
+++ src/main/java/ggoma/ggoma/controller/JwtLoginController.java
@@ -5,6 +5,7 @@ import ggoma.ggoma.entity.UserEntity;
 import ggoma.ggoma.jwt.JwtTokenUtil;
 import ggoma.ggoma.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +24,12 @@ public class JwtLoginController {
             return null;
         }
 
-        String secretKey = "${JWT_SECRET}";
+        String secretKey = "e3a3f9aa7bfd221d40aab4e1112d3a00e70b76c02bf756d1b88225c573535e0a";
 
         // token 유효 시간(60분)
         long expireTimeMs = 1000 * 60 * 60;
 
+        //MemberNumber 값을 SnsId 값으로 수정 필요!!!!!
         String jwtToken = JwtTokenUtil.createToken(userEntity.getMemberNumber(), secretKey, expireTimeMs);
 
         return jwtToken;
